7c999193f1827752e33be9190e5f4646bdf1564e
reconrus <reconrus@yandex.ru>
2019-04-18 02:06:26 +0300

    task3

---
+++ task3/task3.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import re
+
+
+PATH_TO_DRIVER = 'chromedriver.exe'
+KEYWORDS = "python qt"
+
+def popular_repositories(keywords, driver_path):
+
+    URL = "https://github.com/search?q=" + '+'.join(keywords.split())
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome(executable_path=driver_path, chrome_options=options)
+
+    driver.get(URL)
+
+    WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//a[contains(@data-hydro-click, '\"result_position\":10')]"))
+    )
+
+    links = []
+
+    repos = driver.find_elements_by_xpath("//a[contains(@data-hydro-click, '\"result_position\"')]")
+    
+    for i in range(5): 
+        links.append(repos[i].get_attribute("href"))
+
+    driver.quit()
+
+    return links
+
+if __name__ == "__main__":
+    print(popular_repositories(KEYWORDS, PATH_TO_DRIVER))
